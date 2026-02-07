e527867d0aa13fc6f91cbf05ca5d3538da668a1d
Justin Yian Liao <jliao@roblox.com>
2026-02-06 19:32:27 -0800

    Rename

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/jl-vday-2024/',
+  base: '/jl-vday-2026/',
   plugins: [react()],
   build: {
     target: 'es2022',
